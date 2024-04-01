61d7729ec97bdb68a996065a917e3d9f9c825884
Mescareid <catalinburlacu50@yahoo.com>
2024-04-01 21:52:15 +0300

    added lab2 excel

--- src/test/java/tasks/model/TaskTest.java
+++ src/test/java/tasks/model/TaskTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,11 +13,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaskTest {
 
     private Task task;
+    private Date start, end;
+    private SimpleDateFormat sdf;
+
 
     @BeforeEach
     void setUp() {
+        sdf= Task.getDateFormat();
         try {
-            task=new Task("new task",Task.getDateFormat().parse("2023-02-12 10:10"));
+            start=sdf.parse("2023-02-27 12:00");
+            end=sdf.parse("2023-02-27 10:00");
+        } catch (ParseException e) {
+            fail(e.getMessage());
+        }
+
+        try {
+            task=new Task("pasionatii", Task.getDateFormat().parse("2024-04-01 10:10"));
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -24,13 +37,77 @@ class TaskTest {
 
     @Test
     void testTaskCreation() throws ParseException {
-       assert task.getTitle() == "new task";
+       assert task.getTitle() == "pasionatii";
         System.out.println(task.getFormattedDateStart());
-        System.out.println(task.getDateFormat().format(Task.getDateFormat().parse("2023-02-12 10:10")));
-       assert task.getFormattedDateStart().equals(task.getDateFormat().format(Task.getDateFormat().parse("2023-02-12 10:10")));
+        System.out.println(task.getDateFormat().format(Task.getDateFormat().parse("2024-04-01 10:10")));
+       assert task.getFormattedDateStart().equals(task.getDateFormat().format(Task.getDateFormat().parse("2024-04-01 10:10")));
     }
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void getDateFormat() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void isActive() {
+    }
+
+    @Test
+    void setActive() {
+    }
+
+    @Test
+    void getTime() {
+    }
+
+    @Test
+    void setTime() {
+    }
+
+    @Test
+    void getStartTime() {
+    }
+
+    @Test
+    void getEndTime() {
+    }
+
+    @Test
+    void getRepeatInterval() {
+    }
+
+    @Test
+    void testSetTime() {
+    }
+
+    @Test
+    void isRepeated() {
+    }
+
+    @Test
+    void nextTimeAfter() {
+    }
+
+    @Test
+    void getFormattedDateStart() {
+    }
+
+    @Test
+    void getFormattedDateEnd() {
+    }
+
+    @Test
+    void getFormattedRepeated() {
     }
 }
